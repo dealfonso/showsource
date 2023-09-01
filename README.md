8a9774c6bcd886bbe532a0629279538220bb7bd2
Carlos A <caralla@upv.es>
2023-09-02 00:23:57 +0200

    first public version

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
-# jscodefromcode
-Generates code snippets from the existing code in a web page. It is ideal to create pages with examples
+# Extract HTML code from HTML documents (showsource)
+
+This library enables to extract HTML code from the current HTML document, and to add it to the current document in the form of source code.
+
+The library is useful for showing the source code of the current HTML document, and its origin was to ease the creation of web pages for examples of HTML and CSS fragments. My problem was that I wanted to show the example and the code that generated it, but I did not want to maintain two separate code-fragments. This library solves this problem by extracting the HTML code from an element from the current document and adding it to the document in the form of source code.
+
+## Use case
+
+The idea is that if we include the following code in the HTML document:
+
+```html
+<div class="example" data-showsource>
+    <h2>Simple example</h2>
+    <p><span class="fw-bold">hint:</span> add the <span class="fw-bold">data-showsource</span> attribute to the element that we want to extract the html content and to add the result after it.</p>
+    <p>the default behavior is to hide the root element (in this case, <span class="fw-bold">data-showsource</span>)</p>
+    <div class="d-flex">
+        <img src="https://picsum.photos/400/200" class="mx-auto">
+    </div>
+</div>
+```
+
+The result will be as shown in the image below: we'll have a block with a title, the texts, and the image. By including the attribute `data-showsource`, in the envelope (i.e. the _example_ `div`), we'll have the source code of the block after it.
+
+![Simple example](img/example.png)
+
+Once generated, we can use the library [highlightjs](https://highlightjs.org/) to highlight the source code (but this library does not require it).
+
+## Usage
+
+The library is available in the form of a CDN, so we can include it in our HTML document as follows:
+
+```html
+<script src="https://cdn.jsdelivr.net/gh/dealfonso/showsource@1.0/showsource.min.js"></script>
+```
+
+The library will discover any element with the `data-showsource` attribute and will add the source code of the element after it.
+
+### Declarative
+
+The library can be used in a declarative way, by adding the `data-showsource` attribute to the element that we want to extract the HTML content and to add the result after it. A simple example is shown below:
+
+```html
+<div class="example" data-showsource>
+    <h2>Simple example</h2>
+    <div class="d-flex">
+        <img src="https://picsum.photos/400/200" class="mx-auto">
+    </div>
+</div>
+```
+
+### Javascript API
+
+It is also possible to use the library from Javascript. So, we can use it to extract the HTML code from an element in a beautiful format (i.e. with indentation and line breaks). Then we can use the result to show it in a dialog, or to add it to the document in a different way.
+
+The library will be available in the global variable `showsource` with the following methods:
+- `showsource.beautify(el, userOptions = {}, indent = "")` - returns a list of strings with the HTML source code of the element `el` and its children. The `userOptions` parameter is optional and allows to customize the behavior of the function. The `indent` parameter is optional and is used to indent the source code.
+
+- `showsource.init()` - discovers any element with the `data-showsource` attribute and adds the source code of the element after it. The function returns the list of elements that have been processed.
+
+    This method is called automatically when the library is loaded, but it can be called again if we add new elements with the `data-showsource` attribute.
+
+An example of usage is shown below:
+
+```javascript
+// get the element that we want to extract the html content
+const el = document.querySelector(".example");
+let lines = showsource.beautify(el);
+
+// add the source code to the document
+el.insertAdjacentHTML("afterend", `<pre><code>${lines.join("\n")}</code></pre>`);
+```
+
+### Options
+
+The `showsource.beautify` method accepts an optional `userOptions` parameter that allows to customize the behavior of the function. The following options are available:
+
+```javascript
+{
+// The indentation to be used (the spaces at the beginning of the line) (inheritable)
+indentation: "  ",
+// Remove the children of these types of elements
+remove: "h1 h2 h3 h4 h5 h6 p",
+// Hide the element (i.e. the tag) itself, but not its children
+hide: false,
+// Skip the element, along with its children
+skip: false,
+// Skip the children of the element, but not the element itself
+skipChildren: false,
+// Hide the attributes related to this plugin (inheritable)
+hidePlugin: true,
+// The attributes to be removed from the element
+removeAttributes: null,
+// The class attribute to add to the main container div
+class: "showsource"
+}
+```
+
+The options can also be used in the declarative way, by adding the `data-showsource-*` attributes to the element that we want to extract the HTML content and to add the result after it.
+
+The possible options are:
+- `data-showsource-indentation` is the indentation to be used (the spaces at the beginning of the line), that will be accumulated when rendering the children. The default value is two spaces, so that the source corresponding to the children of the element will have two extra spaces at the beginning. The value is inheritable to the children.
+- `data-showsource-remove` is a space separated list of html elements (or selectors) to be removed from the element. The default value is `h1 h2 h3 h4 h5 h6 p`.
+- `data-showsource-hide` instructs the library to hide the render of the current element (i.e. the tag) itself, but not its children. The children will be rendered at the level of the current element. The default value is `false`.
+- `data-showsource-skip` instructs the library to skip the element, along with its children. The default value is `false`.
+- `data-showsource-skip-children` instruct the library to the children of the element, but not the element itself (unless the `data-showsource-hide` attribute is also present). The default value is `false`.
+- `data-showsource-hide-plugin` instructs the library to hide the attributes related to this plugin (i.e. the `data-showsource-*` attributes). The default value is `true`. This attribute is inheritable to the children.
+- `data-showsource-remove-attributes` is a space separated list of the attributes to be hidden from the element. The default value is `null`.
+- `data-showsource-class` is a space separated list of classes to add to the main container div. The default value is `showsource`.
---
+++ js/showsource.js
@@ -0,0 +1,225 @@
+/**
+    MIT License
+
+    Copyright (c) 2023 Carlos de Alfonso (https://github.com/dealfonso)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+defaultOptions = {
+    // The indentation to be used (the spaces at the beginning of the line) (inheritable)
+    indentation: "  ",
+    // Remove the children of these types of elements
+    remove: "h1 h2 h3 h4 h5 h6 p",
+    // Hide the element (i.e. the tag) itself, but not its children
+    hide: false,
+    // Skip the element, along with its children
+    skip: false,
+    // Skip the children of the element, but not the element itself
+    skipChildren: false,
+    // Hide the attributes related to this plugin (inheritable)
+    hidePlugin: true,
+    // The attributes to be removed from the element
+    removeAttributes: null,
+    // The class attribute to add to the main container div
+    class: "showsource"
+};
+
+/**
+ * Obtains the definition of an HTML element, and returns it as a string in a beautiful format
+ * @param {*} element, the element to be beautified
+ * @param {*} indent, the indentation to be used (the spaces at the beginning of the line)
+ * @returns 
+ */
+function beautify(el, userOptions = {}, indent = "") {
+
+    let elOptions = {};
+    if (el.dataset.showsourceIndentation !== undefined) {
+        elOptions.indentation = el.dataset.showsourceIndentation;
+    }
+
+    if (el.dataset.showsourceRemove !== undefined) {
+        elOptions.remove = el.dataset.showsourceRemove;
+    }
+
+    if (el.dataset.showsourceHide !== undefined) {
+        elOptions.hide = el.dataset.showsourceHide.toLowerCase() != "false";
+    }
+
+    if (el.dataset.showsourceSkip !== undefined) {
+        elOptions.skip = el.dataset.showsourceSkip.toLowerCase() != "false";
+    }
+
+    if (el.dataset.showsourceSkipChildren !== undefined) {
+        elOptions.skipChildren = el.dataset.showsourceSkipChildren.toLowerCase() != "false";
+    }
+
+    if (el.dataset.showsourceHidePlugin !== undefined) {
+        elOptions.hidePlugin = el.dataset.showsourceHidePlugin.toLowerCase() != "false";
+    }
+
+    let options = Object.assign({}, defaultOptions, elOptions, userOptions);
+
+    if (options.skip) {
+        return [];
+    }
+
+    // We are cloning the element, so that we can remove it from the DOM, without affecting the original element
+    el = el.cloneNode(true);
+
+    // The inherited options will be passed to the children
+    let childOptions = Object.assign({}, userOptions, {
+        indentation: options.indentation,
+        hidePlugin: options.hidePlugin
+    });
+
+    if (typeof options.remove === "string") {
+        // We'll remove the elements that the user wants to remove
+        let elementsRemove = options.remove.split(" ").join(",");
+        if (elementsRemove.trim() != "") {
+            el.querySelectorAll(elementsRemove).forEach(function (e) {
+                e.remove();
+            });
+        }
+    }
+
+    let beautifulElement = [];
+
+    if (!options.hide) {
+        beautifulElement.push(indent + "<" + el.tagName.toLowerCase());
+        // We'll add the attributes to the tag, but after each "data-" attribute, we'll add a new line
+        let isData = false;
+        let removeAttributes = [];
+        if (typeof options.removeAttributes === "string") {
+            removeAttributes = options.removeAttributes.split(" ");
+        }
+        for (attribute of el.attributes) {
+            if (attribute.name.startsWith("data-showsource")) {
+                if (options.hidePlugin) {
+                    continue;
+                }
+            }
+            if (removeAttributes.includes(attribute.name)) {
+                continue;
+            }
+            if (attribute.name.startsWith("data-") || isData) {
+                beautifulElement.push("  " + indent + attribute.name + '="' + attribute.value + '"');
+            } else {
+                beautifulElement[beautifulElement.length - 1] += " " + attribute.name + '="' + attribute.value + '"';
+            }
+            isData = attribute.name.startsWith("data-");
+        }
+    }
+
+    // We are checking if the element has children (different from text nodes; that is why we are using child here, and not childNodes)
+    if ((el.children.length == 0) && (!options.hide)) {
+        let selfClosingTags = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"];
+        if (selfClosingTags.includes(el.tagName.toLowerCase())) {
+            // If it is a self-closing tag, we'll not add the closing tag (i.e. </...>)
+            beautifulElement[beautifulElement.length - 1] += ">" + el.innerHTML.trim();
+        } else {
+            let literalTags = [ "script", "style" ];
+            if (literalTags.includes(el.tagName.toLowerCase())) {
+                beautifulElement[beautifulElement.length - 1] += ">";
+
+                // Remove blank lines from the beginning and the end
+                let lines = el.innerHTML.split("\n");
+                while (lines.length > 0 && lines[0].trim() == "") {
+                    lines.shift();
+                }
+                while (lines.length > 0 && lines[lines.length - 1].trim() == "") {
+                    lines.pop();
+                }
+                beautifulElement.push(lines.join("\n"));
+                beautifulElement.push("</" + el.tagName.toLowerCase() + ">");
+            } else {
+                // If we have separated the tag in multiple lines, we'll add the closing tag in a new line; otherwise, we'll add it in the same line
+                if (beautifulElement.length > 1) {
+                    beautifulElement[beautifulElement.length - 1] += ">" + el.innerHTML.trim()
+                    beautifulElement.push(indent + "</" + el.tagName.toLowerCase() + ">");
+                } else {
+                    beautifulElement[beautifulElement.length - 1] += ">" + el.innerHTML.trim() + "</" + el.tagName.toLowerCase() + ">";
+                }
+            }
+        }
+    } else {
+        if (!options.hide) {
+            // If the element has children, we'll add the beautiful definition of the children
+            beautifulElement[beautifulElement.length - 1] += ">";
+        }
+        if (!options.skipChildren) {
+            for (child of el.childNodes) {
+                if (child.nodeType == Node.TEXT_NODE) {
+                    let text = child.textContent.trim();
+                    if (text !== "") {
+                        beautifulElement.push(indent + options.indentation + text);
+                    }
+                } else {
+                    let beautifulChild = beautify(child, childOptions, indent + (options.hide?"":options.indentation));
+                    beautifulElement = beautifulElement.concat(beautifulChild);
+                }
+            }
+        }
+        if (!options.hide) {
+            beautifulElement.push(indent + "</" + el.tagName.toLowerCase() + ">");
+        }
+    }
+
+    // Remove the cloned element from the DOM
+    el.remove();
+    return beautifulElement;
+}
+
+function init() {
+    document.querySelectorAll("div[data-showsource]").forEach(function (el) {
+
+        let classString = defaultOptions.class;
+        if (el.dataset.showsourceClass !== undefined) {
+            if (typeof el.dataset.showsourceClass === "string") {
+                classString = el.dataset.showsourceClass;
+            } else {
+                console.error("The value of the data-showsource-class attribute must be a string");
+            }
+        }
+
+        let container = document.createElement("div");
+        let pre = document.createElement("pre");
+        let code = document.createElement("code");
+        code.textContent = beautify(el).join("\n");
+        pre.appendChild(code);
+        container.appendChild(pre);
+
+        classString.split(" ").forEach(function (c) {
+            container.classList.add(c);
+        });
+
+        el.parentNode.insertBefore(container, el.nextSibling);
+    });
+}
+
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function () {
+        init();
+    });
+}
+
+window.showsource = {
+    beautify: beautify,
+    init: init
+}
